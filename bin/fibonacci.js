28f5138fdc39186458c8060d5cac0afa55ba3b92
laurenfazah <laurenfazah@gmail.com>
2016-08-01 21:00:52 -0400

    Fibonacci solutions in

--- bin/fibonacci.js
+++ bin/fibonacci.js
@@ -1,1 +1,34 @@
 'use strict';
+
+let fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+
+///////
+// Code Along: reduce
+// sum
+let sumFibs = fibs.reduce(function(prev,curr){
+  return prev + curr;
+}, 0);
+
+///////
+// Lab: Fibonacci Calculations
+//////
+// product
+let prodFibs = fibs.slice(1,fibs.length).reduce(function(prev,curr){
+  return prev * curr;
+}, 1);
+
+// sum odd
+let sumOdd = fibs.filter(function(val){
+  return val % 2 !== 0;
+})
+.reduce(function(prev,curr){
+  return prev + curr;
+}, 0);
+
+// prod even
+let prodEven = fibs.filter(function(val){
+  return val % 2 === 0 && val > 0;
+})
+.reduce(function(prev,curr){
+  return prev * curr;
+}, 1);
--- bin/fibonacci.rb
+++ bin/fibonacci.rb
@@ -1,1 +1,51 @@
 fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+# def times_two(number)
+#   number * 2
+# end
+
+# twice_fibs = fibs.map { |fib| times_two(fib) }
+# twice_fibs = fibs.map { |fib| fib * 2}
+
+# fibs.map do |fib|
+#   fib * 2
+# end
+
+#####
+# Code Along: reduce
+#####
+# sum
+sum_fibs = fibs.reduce(0) { |accumulator, element| accumulator + element }
+
+# other syntax
+sum_fibs_other = fibs.reduce(:+)
+
+
+#####
+# Lab: Fibonacci Calculations
+#####
+# product
+prod_fibs =
+  fibs.slice(1,fibs.length).reduce(1) { |accumulator, element| accumulator * element }
+
+# other syntax
+prod_fibs_other =
+  fibs.slice(1,fibs.length).reduce(:*)
+
+
+# sum odd
+sum_odd =
+  fibs.select { |num| num.odd? }.reduce(0) { |accumulator, element| accumulator + element }
+
+# other syntax
+sum_odd_other =
+  fibs.select { |num| num.odd? }.reduce(:+)
+
+
+# prod even
+prod_even =
+  fibs.select { |num| num.even? && num > 0 }.reduce(1) { |accumulator, element| accumulator * element }
+
+# other syntax
+prod_even_other =
+  fibs.select { |num| num.even? && num > 0 }.reduce(:*)
