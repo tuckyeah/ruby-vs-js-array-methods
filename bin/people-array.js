610091c5c1853c9a0cf9c302d7034c41b80644cd
laurenfazah <laurenfazah@gmail.com>
2016-08-01 21:15:50 -0400

    People lab and code along solutions in

--- bin/people-array.js
+++ bin/people-array.js
@@ -2,7 +2,32 @@
 
 let loadPeople = require('../lib/people.js');
 
+// loadPeople().then((people) => {
+//   let count = people.reduce((m /*, p */) => m + 1, 0);
+//   console.log(`count: ${count}, length: ${people.length}`);
+// }).catch((error) => console.log(error));
+
+
 loadPeople().then((people) => {
-  let count = people.reduce((m /*, p */) => m + 1, 0);
-  console.log(`count: ${count}, length: ${people.length}`);
+  // people is array of objects
+  // counting total length:
+  let count = people.reduce((memo, person) => {
+    return memo + 1;
+  }, 0);
+
+  // callback for reduce
+  const isFemale = (person) => {
+    return person.gender === "f";
+  };
+
+  // counting total women
+  let female = people.filter(isFemale).reduce((memo, person) => {
+    return memo + 1;
+  }, 0);
+
+  console.log(`female: ${female}, total: ${count}`);
+
+  // people.forEach((person) => {
+  //   console.log(`${person.given_name} ${person.surname} is ${person.age()} years old.`);
+  // });
 }).catch((error) => console.log(error));
--- bin/people_array.rb
+++ bin/people_array.rb
@@ -3,3 +3,33 @@ require_relative '../lib/people.rb'
 people = People.new.people
 
 p people.count
+
+######
+# Code Along: People
+######
+total = people.count.to_f
+
+female = people.select { |person| person.gender == 'f' }.length
+female_percentage = female / total
+p "female total: #{female}, total: #{total},
+  female_percentage: #{female_percentage}"
+
+######
+# Lab: People
+######
+older_people = people.select { |person| person.age > 26 }.length
+p "older people: #{older_people}"
+
+younger_people = people.select { |person| person.age < 26 }.length
+p "younger people: #{younger_people}"
+
+letter_matches = people.select { |person| person.given_name[0] == person.surname[0] }.length
+p "letter_matches: #{letter_matches}"
+
+letter_matches_people =
+  people.select { |person| person.given_name[0] == person.surname[0] }
+
+average_age =
+  people.reduce(0) { |accumulator, element| accumulator + element.age } / total
+
+p "avg age: #{average_age}"
