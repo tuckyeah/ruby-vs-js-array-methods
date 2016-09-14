8c81125d5f293cf9ccec8d610d32fc854ff3decc
tuckyeah <h.rosebrock@gmail.com>
2016-09-14 14:17:23 -0400

    my squad solutions for the lab

--- bin/fibonacci.js
+++ bin/fibonacci.js
@@ -1,1 +1,52 @@
 'use strict';
+
+let fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+// sum of all the elements of the fibs array
+let sumFibs = fibs.reduce(sum, 0);
+
+console.log('The sum of the first ten fibonacci numbers is ', sumFibs);
+
+
+// const notZero = (number) => {
+//   return number !== 0;
+// };
+//
+// let prodFibs = fibs.filter(notZero).reduce(product, 1)
+// product of all elements in fibs array
+const product = (a, b) => {
+  if (a > 0 && b > 0) {
+    return a * b;
+  } else {
+    return b * 1;
+  }
+};
+
+let prodFibs = fibs.reduce(product, 1);
+
+console.log('The product of the first ten fibonacci numbers is ', prodFibs);
+
+// sum of odd elements of fibs
+let isOdd = function(index) {
+  return index % 2 !== 0;
+};
+
+let oddFibs = fibs.filter(isOdd).reduce(sum, 0);
+
+console.log("The sum of the odd elements of fibonacci is ", oddFibs);
+
+let isEven = function(index) {
+  return index % 2 === 0;
+};
+
+let evenFibs = fibs.filter(isEven).reduce(product, 1);
+
+console.log("The product of the even elements of fibonacci is", evenFibs);
+
+module.exports = {
+  sumFibs,
+};
--- bin/fibonacci.rb
+++ bin/fibonacci.rb
@@ -1,1 +1,12 @@
 fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+
+#sum of all the elements in the fibs array
+sum_fibs = fibs.reduce(0, :+)
+
+puts "The sum of the first ten fibonacci numbers is #{sum_fibs}"
+
+#product of all the elements in the fibs array
+prod_fibs = fibs[1..-1].reduce(:*)
+
+puts "The product of the first ten fibonacci numbers is #{prod_fibs}"
